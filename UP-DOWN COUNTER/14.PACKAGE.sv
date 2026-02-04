5425201631720f193a3f67b24e75528837fe124e
DHIVAGAR.T <dhivagardhanavel07@gmail.com>
2026-02-04 23:48:57 +0530

    Create 14.PACKAGE.sv

---
+++ UP-DOWN COUNTER/14.PACKAGE.sv
@@ -0,0 +1,15 @@
+
+  `include "uvm_macros.svh"
+
+  `include "sequence_item.sv"
+  `include "sequence.sv"
+  `include "sequencer.sv"
+  `include "driver.sv"
+  `include "monitor.sv"
+  `include "agent.sv"
+  `include "scoreboard.sv"
+  `include "coverage.sv"
+  `include "environment.sv"
+  `include "test.sv"
+
+
