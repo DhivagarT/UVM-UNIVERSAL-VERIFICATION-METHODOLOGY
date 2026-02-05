499a0d74a5909c65cce961c417ba303ca7dfa739
DHIVAGAR.T <dhivagardhanavel07@gmail.com>
2026-02-05 23:50:45 +0530

    Create 05.SEQUENCER.sv

---
+++ D - FLIP_FLOP/05.SEQUENCER.sv
@@ -0,0 +1,23 @@
+`ifndef SEQUENCER_SV
+`define SEQUENCER_SV
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+class sequencer extends uvm_sequencer#(transaction);
+  `uvm_component_utils(sequencer)
+  
+  function new(string name="sequencer",uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    
+    `uvm_info("SEQ","SEQUENCER BUILD PHASE STARTED",UVM_MEDIUM)
+    
+  endfunction
+endclass
+`endif
+
+    
