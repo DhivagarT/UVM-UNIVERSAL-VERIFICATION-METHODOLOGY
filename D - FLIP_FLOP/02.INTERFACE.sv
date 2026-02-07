b78695dfb4bcffa9f66c27665c3430a6a0a68ca8
DHIVAGAR.T <dhivagardhanavel07@gmail.com>
2026-02-07 17:15:50 +0530

    Refactor interface by removing din and qout
    Removed unused signals and clocking block.

--- D - FLIP_FLOP/02.INTERFACE.sv
+++ D - FLIP_FLOP/02.INTERFACE.sv
@@ -1,13 +1,7 @@
 interface intf();
   logic clk;
   logic rst;
-  logic din;
-  logic qout;
-  
-//   modport dut(input clk,rst,din,output dout);
-  clocking cb@(posedge clk);
-    input qout;
-    output rst,din;
-  endclocking
+  logic d;
+  logic q;
   
 endinterface
