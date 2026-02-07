713a6fe6508b7e2d056dad8f600a79faef317b81
DHIVAGAR.T <dhivagardhanavel07@gmail.com>
2026-02-07 17:16:59 +0530

    Remove unnecessary preprocessor directives and build_phase

--- D - FLIP_FLOP/05.SEQUENCER.sv
+++ D - FLIP_FLOP/05.SEQUENCER.sv
@@ -1,6 +1,3 @@
-`ifndef SEQUENCER_SV
-`define SEQUENCER_SV
-
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
@@ -10,14 +7,4 @@ class sequencer extends uvm_sequencer#(transaction);
   function new(string name="sequencer",uvm_component parent=null);
     super.new(name,parent);
   endfunction
-  
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    
-    `uvm_info("SEQ","SEQUENCER BUILD PHASE STARTED",UVM_MEDIUM)
-    
-  endfunction
 endclass
-`endif
-
-    
