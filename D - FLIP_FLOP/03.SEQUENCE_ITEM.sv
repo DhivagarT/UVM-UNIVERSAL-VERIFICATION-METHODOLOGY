9e2535c10d5b1e16a36c42dae195f3fa18c9d24e
DHIVAGAR.T <dhivagardhanavel07@gmail.com>
2026-02-07 17:16:09 +0530

    Update 03.SEQUENCE_ITEM.sv

--- D - FLIP_FLOP/03.SEQUENCE_ITEM.sv
+++ D - FLIP_FLOP/03.SEQUENCE_ITEM.sv
@@ -1,30 +1,16 @@
-`ifndef SEQUENCE_ITEM
-`define SEQUENCE_ITEM
-
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
 class transaction extends uvm_sequence_item;
+  `uvm_object_utils(transaction)
   
-  rand bit rst;
-  rand bit din;
-  bit qout;
-  
-  `uvm_object_utils_begin(transaction)
-  `uvm_field_int(rst,UVM_ALL_ON)
-  `uvm_field_int(din,UVM_ALL_ON)
-  `uvm_field_int(qout,UVM_ALL_ON)
-  `uvm_object_utils_end
-  
+   rand bit rst;
+  rand bit d;
+  bit q;
   
   function new(string name="transaction");
     super.new(name);
+    
   endfunction
   
-  constraint c1{din dist{0:=25,1:=25};}
-  
 endclass
-`endif
-
-  
-  
